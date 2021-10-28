7f886ecc273ce3cd92a6944fe413e86d52a1df05
Marc Solèr <marc.soler@docworld.ch>
2021-10-28 13:25:24 +0200

    adds redux

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,19 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+import { Provider  } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/store/reducers/postReducer.tsx
@@ -0,0 +1,7 @@
+const initState = {};
+
+const postReducer = (state = initState, action: any) => {
+    return state;
+}
+
+export default postReducer;
