303d4f27fd77948cd067abe72996edf10aa6cda9
Marc Solèr <marc.soler@docworld.ch>
2021-11-05 11:42:45 +0100

    navbar implements react-bootstrap

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,32 +3,30 @@ import {Link} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../store';
 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 import SingedInLinks from './navigation/SingedInLinks';
 import SignedOutLinks from './navigation/SignedOutLinks';
 
 const Header: FC = () => {
 
 
-    const { authenticated } = useSelector((state: RootState) => state.auth);
+    const {authenticated} = useSelector((state: RootState) => state.auth);
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
+        <Navbar bg="light" expand="lg">
+            <Container>
                 <Link className="navbar-brand" to="/">Triplog</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"/>
-                </button>
-                <div className="collapse navbar-collapse" id="mainNavbar">
-                    <ul className="navbar-nav me-auto">
-                    </ul>
-                    <ul className="navbar-nav">
-                        { authenticated ? <SingedInLinks /> : <SignedOutLinks /> }
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse className="justify-content-end" id="navbar-nav">
+                    <Nav>
+                        {authenticated ? <SingedInLinks/> : <SignedOutLinks/>}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
