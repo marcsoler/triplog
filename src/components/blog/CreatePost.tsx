40604d9be2404e778750574b9e129b4216a30b0c
Marc Solèr <marcsoler87@gmail.com>
2021-11-29 00:54:28 +0100

    wip: forms

--- src/components/blog/CreatePost.tsx
+++ src/components/blog/CreatePost.tsx
@@ -1,5 +1,6 @@
-import {FC} from 'react';
+import {FC, FormEvent, useState} from 'react';
 
+import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -7,12 +8,51 @@ import Row from 'react-bootstrap/Row';
 
 
 const CreatePost: FC = () => {
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+    }
+
     return (
         <Container>
             <Row>
                 <Col xs={12} md={10} lg={8}>
+
+                    <h2>New post</h2>
+
                     <Form>
-                        <h2>New post</h2>
+
+                        <Form.Group className="mb-3" controlId="title">
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" required />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Slug</Form.Label>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Group className="mb-3" controlId="subtitle">
+                                <Form.Label>Subtitle</Form.Label>
+                                <Form.Control type="subtitle" />
+                            </Form.Group>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="content">
+                            <Form.Label>Content</Form.Label>
+                            <Form.Control as="textarea" rows={10} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="status">
+                            <Form.Label>Status</Form.Label>
+                            <Form.Check type="switch" id="status-switch" label="Publish" />
+                        </Form.Group>
+
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
                     </Form>
                 </Col>
             </Row>
