53f46ffa4e86beb1fa2709942ec47e11af10b61e
Marc Solèr <marcsoler87@gmail.com>
2021-12-02 02:13:41 +0100

    basic post submission to fb

--- src/components/blog/CreatePost.tsx
+++ src/components/blog/CreatePost.tsx
@@ -1,18 +1,49 @@
-import {FC, FormEvent, useState} from 'react';
+import {FC, FormEvent, useState, useEffect} from 'react';
+
+import slugify from 'slugify';
 
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import InputGroup from 'react-bootstrap/InputGroup';
+import {useDispatch} from 'react-redux';
+import {createPost} from '../../store/actions/postActions';
 
 
 const CreatePost: FC = () => {
 
+    const [title, setTitle] = useState('');
+    const [slug, setSlug] = useState('');
+    const [subtitle, setSubtitle] = useState('');
+    const [content, setContent] = useState('');
+    const [status, setStatus] = useState('');
+
+    const dispatch = useDispatch();
+
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
 
+        console.log(title, slug, subtitle, content, status);
+
+        dispatch(createPost({
+            title,
+            slug,
+            subtitle,
+            content, status
+        }, () => console.log('hmm?')));
+
     }
+
+    useEffect(() => {
+        return () => {
+            setSlug(slugify(title, {
+                lower: true
+            }));
+        };
+    }, [title]);
+
 
     return (
         <Container>
@@ -21,32 +52,35 @@ const CreatePost: FC = () => {
 
                     <h2>New post</h2>
 
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
 
                         <Form.Group className="mb-3" controlId="title">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control type="text" required />
+                            <Form.Control type="text" required onChange={(e) => setTitle(e.currentTarget.value)} />
                         </Form.Group>
 
-                        <Form.Group>
+                        <Form.Group className="mb-3">
                             <Form.Label>Slug</Form.Label>
+                            <InputGroup>
+                                <InputGroup.Text>{`${window.location.origin}/post/`}</InputGroup.Text>
+                                <Form.Control type="text" value={slug} readOnly/>
+                            </InputGroup>
                         </Form.Group>
 
-                        <Form.Group>
-                            <Form.Group className="mb-3" controlId="subtitle">
-                                <Form.Label>Subtitle</Form.Label>
-                                <Form.Control type="subtitle" />
-                            </Form.Group>
+                        <Form.Group className="mb-3" controlId="subtitle">
+                            <Form.Label>Subtitle</Form.Label>
+                            <Form.Control type="subtitle" onChange={(e) => setSubtitle(e.currentTarget.value)}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="content">
                             <Form.Label>Content</Form.Label>
-                            <Form.Control as="textarea" rows={10} />
+                            <Form.Control as="textarea" rows={10} onChange={(e) => setContent(e.currentTarget.value)}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="status">
                             <Form.Label>Status</Form.Label>
-                            <Form.Check type="switch" id="status-switch" label="Publish" />
+                            <Form.Check type="switch" onChange={(e) => setStatus(e.currentTarget.value)}
+                                        label="Publish"/>
                         </Form.Group>
 
 
