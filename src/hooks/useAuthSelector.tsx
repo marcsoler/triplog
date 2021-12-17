7726137b00d7d0f66d4dee3916b76aca97682a51
Marc Solèr <marc.soler@docworld.ch>
2021-12-17 11:32:55 +0100

    adds custom hooks

---
+++ src/hooks/useAuthSelector.tsx
@@ -0,0 +1,19 @@
+import { Auth, onAuthStateChanged, User } from 'firebase/auth';
+import { useEffect, useMemo } from 'react';
+import { LoadingHook, useLoadingValue } from '../util';
+
+export type AuthStateHook = LoadingHook<User | null, Error>;
+
+export default (auth: Auth): AuthStateHook => {
+    const { error, loading, setError, setValue, value } = useLoadingValue<
+        User | null,
+        Error
+        >(() => auth.currentUser);
+
+    useEffect(() => {
+        return onAuthStateChanged(auth, setValue, setError);
+    }, [auth]);
+
+    const resArray: AuthStateHook = [value, loading, error];
+    return useMemo<AuthStateHook>(() => resArray, resArray);
+};
