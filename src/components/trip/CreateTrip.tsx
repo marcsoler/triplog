cc566d60ea94380651b0dea6d364addb9835da5c
Marc Solèr <marc.soler@docworld.ch>
2021-12-29 18:02:02 +0100

    wip: some methods

--- src/components/trip/CreateTrip.tsx
+++ src/components/trip/CreateTrip.tsx
@@ -9,8 +9,7 @@ import Row from 'react-bootstrap/Row';
 
 
 import Loading from '../misc/Loading';
-
-import {DirectionsRenderer, DirectionsService, GoogleMap, Marker, useJsApiLoader} from '@react-google-maps/api';
+import {DirectionsRenderer, GoogleMap, useJsApiLoader} from '@react-google-maps/api';
 import {SubmitHandler, useForm} from 'react-hook-form';
 
 interface ITripForm {
@@ -27,23 +26,28 @@ const CreateTrip: FC = () => {
     const [mapRef, setMapRef] = useState<google.maps.Map>();
     const [dirRef, setDirRef] = useState<google.maps.DirectionsRenderer>()
     const [travelMode, setTravelMode] = useState<google.maps.TravelMode>()
-
     const [waypoints, setWaypoints] = useState<google.maps.LatLng[]>([]);
-
     const [directionsLoaded, setDirectionsLoaded] = useState(false);
-    const [response, setResponse] = useState(undefined);
+    const [response, setResponse] = useState();
+    const [startMarker, setStartMarker] = useState<google.maps.Marker>();
+    const [distance, setDistance] = useState<number>();
+    const [duration, setDuration] = useState<string>()
 
     const onSubmit: SubmitHandler<ITripForm> = data => {
-        switch (travelMode) {
-            case 'BICYCLING':
-                setTravelMode(google.maps.TravelMode.BICYCLING);
+
+    }
+
+    const updateTravelMode = (mode: string) => {
+        switch (mode) {
+            case 'DRIVING':
+                setTravelMode(google.maps.TravelMode.DRIVING);
                 break;
             case 'WALKING':
                 setTravelMode(google.maps.TravelMode.WALKING);
                 break;
-            case 'DRIVING':
+            case 'BICYCLING':
             default:
-                setTravelMode(google.maps.TravelMode.DRIVING);
+                setTravelMode(google.maps.TravelMode.BICYCLING);
                 break;
         }
     }
@@ -53,36 +57,49 @@ const CreateTrip: FC = () => {
     });
 
     const onMapClick = (e: google.maps.MapMouseEvent): void => {
-
-        //first click ist start/second click is finished...
         const wp = e.latLng;
-
         if (wp && waypoints.length < 2) {
             waypoints.push(wp);
-        }
-
-        if (waypoints.length > 1) {
-            console.log('draw route...');
+            if (waypoints.length === 1) {
+                setStartMarker(new google.maps.Marker({
+                    position: wp,
+                    map: mapRef,
+                    title: 'Starting point',
+                }));
+                return;
+            }
             displayRoute();
         }
     }
 
     const displayRoute = () => {
         const directionService = new google.maps.DirectionsService();
-
         directionService.route({
             origin: waypoints[0],
             destination: waypoints[waypoints.length - 1],
-            travelMode: google.maps.TravelMode.BICYCLING,
+            travelMode: travelMode ? travelMode : google.maps.TravelMode.BICYCLING,
         }, directionCallback);
+        if (startMarker) {
+            startMarker.setMap(null);
+        }
+
     }
 
 
-    const directionCallback = (response: any) => {
-        console.log('directionCallback');
-        if (response.status === 'OK') {
+    const directionCallback = (res: any) => {
+        console.log('directionCallback', res);
+        if (res.status === 'OK') {
             setDirectionsLoaded(true);
-            setResponse(response);
+            setResponse(res);
+
+            if (res) {
+                const route = res.routes[0];
+                // @ts-ignore
+                console.log('routes', route.legs[0]);
+                setDistance(route.legs[0].distance.value);
+                setDuration(route.legs[0].duration.text);
+            }
+
         }
     }
 
@@ -109,11 +126,10 @@ const CreateTrip: FC = () => {
         return (
             <Container>
 
+                <h1>Route planner</h1>
+
                 <Row className="mt-3">
                     <Col md={8}>
-
-                        <h1>Route planner</h1>
-
                         <GoogleMap
                             id="gmap-planner"
                             mapContainerStyle={containerStyle}
@@ -134,7 +150,21 @@ const CreateTrip: FC = () => {
 
                     </Col>
                     <Col as="aside" md={4}>
-                        <h3>Route info</h3>
+                        <Form>
+                            <Form.Select aria-label="Select the transportation mode" onChange={(e) => {
+                                updateTravelMode(e.target.value)
+                            }}>
+                                <option value="BICYCLE">Bicycle</option>
+                                <option value="DRIVING">Driving</option>
+                                <option value="WALKING">Walking</option>
+                            </Form.Select>
+                        </Form>
+
+                        <hr/>
+
+                        {distance && <p><strong>Distance:</strong> {distance}</p>}
+                        {duration && <p><strong>Duration:</strong> {duration}</p>}
+
                     </Col>
                 </Row>
             </Container>
