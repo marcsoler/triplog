54575b97cbad21b59772c6095709618e04f66b48
Marc Solèr <marcsoler87@gmail.com>
2022-01-14 01:32:48 +0100

    improves relates posts & adds missing comment-count

--- src/components/blog/RelatedPost.tsx
+++ src/components/blog/RelatedPost.tsx
@@ -1,20 +1,35 @@
+import {FC, useEffect, useState} from 'react';
+
 import moment from 'moment';
 import {NavLink} from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
-import usePostSelector from '../../hooks/usePostSelector';
+import {Post} from '../../store/types';
+import useCommentsSelector from '../../hooks/useCommentsSelector';
 
-const RelatedPost = ({post}: any) => {
 
-    const activePost = usePostSelector();
+const RelatedPost:FC<Post> = (post: Post) => {
+
+    const [commentCount, setCommentCount] = useState<number>(0);
+
+    const {comments} = useCommentsSelector();
+
+    useEffect(() => {
+        if(comments) {
+            setCommentCount(comments.filter((c) => {
+                return c.post_id === post.slug;
+            }).length);
+        }
+    }, [comments, post]);
+
 
     return (
         <ListGroup variant="flush" className="related-post">
-            <ListGroup.Item as={NavLink} to={'/post/' + post.slug} active={activePost.post && (post.id === activePost.post.slug)}>
+            <ListGroup.Item as={NavLink} to={'/post/' + post.slug}>
                 <div className="d-flex w-100 justify-content-between">
                     <h5 className="mb-1">{post.title}</h5>
                     <small>{moment.unix(post.created_at.seconds).format('MMMM Do YYYY')}</small>
                 </div>
-                <small className="comment-count">(todo) comments</small>
+                <small className="comment-count">{`${commentCount} ${commentCount === 1 ? 'comment' : 'comments'}`}</small>
             </ListGroup.Item>
         </ListGroup>
 
